243aaf09e849ecd39108a2163a7f8224a5588058
DQbryant <1791833099@qq.com>
2020-09-12 21:50:38 +0800

    statistic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,58 @@
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+    static Map<String,Result> userToResult = new HashMap<>();
+    static Map<String,Result> repoToResult = new HashMap<>();
+    static Map<String,Result> userAndRepoToResult = new HashMap<>();
+    static boolean typeCorrect(String type){
+        return "PushEvent".equals(type)||"IssueCommentEvent".equals(type)||"IssuesEvent".equals(type)||"PullRequestEvent".equals(type);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        File file = new File("src/main/java/2015-01-01.json");
+        try {
+            LineIterator it = FileUtils.lineIterator(file,"UTF-8");
+            while (it.hasNext()){
+                JSONObject jsonObject = JSONObject.parseObject(it.nextLine());
+                String type = jsonObject.getString("type");
+                if(typeCorrect(type)){
+                    String username = jsonObject.getJSONObject("actor").getString("login");
+                    Result userResult = userToResult.get(username);
+                    if(userResult==null) {
+                        userResult = new Result();
+                        userResult.inc(type);
+                        userToResult.put(username,userResult);
+                    }else {
+                        userResult.inc(type);
+                    }
+                    String repoName = jsonObject.getJSONObject("repo").getString("name");
+                    Result repoResult = repoToResult.get(repoName);
+                    if(repoResult==null) {
+                        repoResult = new Result();
+                        repoResult.inc(type);
+                        repoToResult.put(repoName,repoResult);
+                    }else {
+                        repoResult.inc(type);
+                    }
+                    String name = username+"_"+repoName;
+                    Result result = userAndRepoToResult.get(name);
+                    if(result==null) {
+                        result = new Result();
+                        result.inc(type);
+                        userAndRepoToResult.put(name,result);
+                    }else {
+                        result.inc(type);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/Result.java
@@ -0,0 +1,37 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author dqbryant
+ * @create 2020/9/10 22:12
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Result {
+    private int pushEventNum;
+    private int issueCommentEventNum;
+    private int issuesEventNum;
+    private int pullRequestEventNum;
+    public void inc(String type){
+        if("PushEvent".equals(type)){
+            this.pushEventNum++;
+        }else if("IssueCommentEvent".equals(type)){
+            this.issueCommentEventNum++;
+        }else if("IssuesEvent".equals(type)){
+            this.issuesEventNum++;
+        }else {
+            this.pullRequestEventNum++;
+        }
+    }
+    @Override
+    public String toString() {
+        return "Result{" +
+                "pushEventNum=" + pushEventNum +
+                ", issueCommentEventNum=" + issueCommentEventNum +
+                ", issuesEventNum=" + issuesEventNum +
+                ", pullRequestEventNum=" + pullRequestEventNum +
+                '}';
+    }
+}
