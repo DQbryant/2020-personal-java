e428ed5bbcb00673bffcf223796833205953cb63
DQbryant <1791833099@qq.com>
2020-09-14 22:17:15 +0800

    map更换和单元测试

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,67 +1,16 @@
-import com.alibaba.fastjson.JSONObject;
-import org.apache.commons.cli.*;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.LineIterator;
 
+import org.apache.commons.cli.*;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Main {
-    static Map<String,Result> userToResult = new HashMap<>();
-    static Map<String,Result> repoToResult = new HashMap<>();
-    static Map<String,Result> userAndRepoToResult = new HashMap<>();
-    static boolean typeCorrect(String type){
-        return "PushEvent".equals(type)||"IssueCommentEvent".equals(type)||"IssuesEvent".equals(type)||"PullRequestEvent".equals(type);
-    }
-    static void init(String path){
-        File file = new File(path);
-        String[] fileNames = file.list();
-        for(String fileName:fileNames){
-            try {
-                LineIterator it = FileUtils.lineIterator(new File(path+"/"+fileName),"UTF-8");
-                while (it.hasNext()){
-                    JSONObject jsonObject = JSONObject.parseObject(it.nextLine());
-                    String type = jsonObject.getString("type");
-                    if(typeCorrect(type)){
-                        String username = jsonObject.getJSONObject("actor").getString("login");
-                        Result userResult = userToResult.get(username);
-                        if(userResult==null) {
-                            userResult = new Result();
-                            userResult.inc(type);
-                            userToResult.put(username,userResult);
-                        }else {
-                            userResult.inc(type);
-                        }
-                        String repoName = jsonObject.getJSONObject("repo").getString("name");
-                        Result repoResult = repoToResult.get(repoName);
-                        if(repoResult==null) {
-                            repoResult = new Result();
-                            repoResult.inc(type);
-                            repoToResult.put(repoName,repoResult);
-                        }else {
-                            repoResult.inc(type);
-                        }
-                        String name = username+"_"+repoName;
-                        Result result = userAndRepoToResult.get(name);
-                        if(result==null) {
-                            result = new Result();
-                            result.inc(type);
-                            userAndRepoToResult.put(name,result);
-                        }else {
-                            result.inc(type);
-                        }
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+    static Map<String,Result> userToResult = new ConcurrentHashMap<>();
+    static Map<String,Result> repoToResult = new ConcurrentHashMap<>();
+    static Map<String,Result> userAndRepoToResult = new ConcurrentHashMap<>();
     static int getPersonalThings(String username,String type){
         Result result = userToResult.get(username);
         return result==null?0:result.getAttribute(type);
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,13 +1,50 @@
 /**
  * @author dqbryant
- * @create 2020/9/13 16:33
  */
 public class Test {
+    /**
+     * 测试初始化
+     */
     @org.junit.Test
     public void testInit(){
-        Main.main(new String[]{"--init","D:/json"});
-        Main.main(new String[]{"-r","tschortsch/gulp-bootlint","-u","tschortsch","-e","PushEvent"});
+        Main.main(new String[]{"--init","D:\\json"});
+    }
+
+    /**
+     * 测试 getPersonalThings
+     * "-u","tschortsch","-e","PushEvent",输出是30
+     */
+    @org.junit.Test
+    public void testGetPersonalThings(){
+        Main.main(new String[]{"--init","D:\\json"});
         Main.main(new String[]{"-u","tschortsch","-e","PushEvent"});
+    }
+
+    /**
+     * 测试 getRepoThings
+     * "-r","fujimura/hi","-e","PushEvent",输出是27
+     */
+    @org.junit.Test
+    public void testGetRepoThings(){
+        Main.main(new String[]{"--init","D:\\json"});
         Main.main(new String[]{"-r","fujimura/hi","-e","PushEvent"});
+    }
+
+    /**
+     * 测试 getPersonalAndRepoThings
+     * "-r","tschortsch/gulp-bootlint","-u","tschortsch","-e","PushEvent",输出是21
+     */
+    @org.junit.Test
+    public void testGetPersonalAndRepoThings(){
+        Main.main(new String[]{"--init","D:\\json"});
+        Main.main(new String[]{"-r","tschortsch/gulp-bootlint","-u","tschortsch","-e","PushEvent"});
+    }
+
+    /**
+     * 测试参数异常: 未传入参数
+     */
+    @org.junit.Test
+    public void testArgs(){
+        Main.main(new String[]{"-u"});
     }
 }
